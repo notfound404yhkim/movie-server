aa4a767be2f129bf0e6f5cc7c1cac0cae0a02437
notfound404yhkim <kyh31445196@gmail.com>
2023-12-13 10:21:37 +0900

    add
    add

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
-from resources.movie import MovieListResource, Movieinfo
+from resources.movie import MovieListResource, MovieResource
 from resources.review import ReviewResource
 
 #  로그 아웃 관련된 임포트문. 
@@ -35,12 +35,9 @@ def check_if_token_is_revoked(jwt_header,jwt_payload):
 api.add_resource(UserRegisterResource ,'/user/register')
 api.add_resource(UserLogoutResource ,'/user/logout')
 api.add_resource(UserLoginResource,'/user/login')
-
-api.add_resource( MovieListResource,'/movie')
-api.add_resource( Movieinfo , '/movie/<int:movie_id>')
-
-
 api.add_resource( ReviewResource , '/review') 
+api.add_resource( MovieResource , '/movie/<int:movie_id>')
+api.add_resource( MovieListResource,'/movie')
 
 if __name__ == '__main__':
     app.run()
--- resources/movie.py
+++ resources/movie.py
@@ -4,43 +4,82 @@ from flask_restful import Resource
 from mysql_connection import get_connection
 from mysql.connector import Error
 
-class MovieListResource(Resource):
-    
-    def get(self):
-        #1 . 클라이언트로부터 데이터를 받아온다.
-        # 없음.
 
-        #2. db에 저장된 데이터를 가져온다.
+class MovieResource(Resource) :
+
+    @jwt_required(optional=True)
+    def get(self, movie_id):
+
+        user_id = get_jwt_identity()
+
+        try : 
+            connection = get_connection()
+            query = '''select m.* , avg(r.rating) as rating_avg , count(r.rating) review_count
+                    from movie m
+                    left join review r
+                    on m.id = r.movieId
+                    where m.id = %s;'''
+            record = (movie_id , )
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query, record)
+            
+            result_list = cursor.fetchall()
+
+            cursor.close()
+            connection.close()        
+        
+        except Error as e:
+            print(e)
+            cursor.close()
+            connection.close()   
+            return {'error' : str(e)}, 500
+        
+        # 영화 상세정보는 result_list의 첫번째 데이터다.
+        # result_list[0]
+
+        print(result_list[0])
+
+        i = 0
+        for row in result_list :
+            result_list[i]['year'] = row['year'].isoformat()
+            result_list[i]['createdAt'] = row['createdAt'].isoformat()
+            result_list[i]['rating_avg'] = float( row['rating_avg'] )
+            i = i + 1
+
+        return {'result' : 'success',
+                'movieInfo' : result_list[0]}
+
+
+class MovieListResource(Resource) :
+
+    @jwt_required()
+    def get(self) :
+
+        order = request.args.get('order')
         offset = request.args.get('offset')
         limit = request.args.get('limit')
-        try:
+
+        user_id = get_jwt_identity()
+
+        try :
             connection = get_connection()
+            query = '''select m.id, m.title , count(r.id) reviewCnt  , 
+                    avg(r.rating) avgRating , 
+                    if( f.id is null , 0 , 1 ) isFavorite
+                    from movie m
+                    left join review r 
+                    on m.id = r.movieId
+                    left join favorite f
+                    on m.id = f.movieId and f.userId = %s
+                    group by m.id
+                    order by '''+order+''' desc
+                    limit '''+offset+''', '''+limit+''';'''
             
-            query = '''select m.title, avg(r.rating) as 'rating_avg',count(r.rating) as 'review_count'
-                        from movie m
-                        left join review r
-                        on m.id = r.movieId
-                        group by m.title
-                        order by rating_avg desc, review_count desc
-                        limit '''+ str(offset) +''', '''+ str(limit) +'''    ;'''
+            record = (user_id , )
 
             cursor = connection.cursor(dictionary=True)
-            cursor.execute(query)
-
-            result_list = cursor.fetchall()  
-
-            print(result_list)
-
-
-            i = 0
-            for row in result_list:
-                result_list[i]['rating_avg'] = str(row['rating_avg'])
-                i = i+1
-
-            print()
-            print(result_list)
-            print()
-                
+            cursor.execute(query, record)
+            result_list = cursor.fetchall()
             cursor.close()
             connection.close()
 
@@ -48,51 +87,14 @@ class MovieListResource(Resource):
             print(e)
             cursor.close()
             connection.close()
-            #클라이언트에게 에러를 보내줘야한다.
-            return {"result" : "fail", "error" : str(e)},500
-        
-
-
-        return {"result " : "success",
-                "itmems" : result_list,
-                "count " : len(result_list)},200
-    
-class Movieinfo(Resource) :
-    
-    def get(self,movie_id) :
-        
-        try :
-            connection = get_connection()
-
-            query = '''select m.*,avg(r.rating) as 'rating_avg',count(r.rating) as 'review_count'
-                        from movie m
-                        left join review r
-                        on m.id = r.movieId
-                        where m.id = %s
-                        group by m.title;'''
-            record = (movie_id , )
-
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(query, record)
-
-            result_list = cursor.fetchall()
-
-            cursor.close()
-            connection.close()
-
-        except Error as e :
-            print(e)
-            cursor.close()
-            connection.close()
             return {'error' : str(e)}, 500
         
-        i = 0
-        for row in result_list:
-            result_list[i]['rating_avg'] = str(row['rating_avg'])
-            result_list[i]['createdAt'] = row['createdAt'].isoformat()
-            result_list[i]['year'] = row['year'].isoformat()
-            i = i+1
+        print(result_list)
 
+        i = 0
+        for row in result_list :
+            result_list[i]['avgRating'] = float( row['avgRating'] )
+            i = i + 1
 
         return {'result' : 'success',
                 'items' : result_list,
